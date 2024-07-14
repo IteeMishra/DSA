faeeb41e81e5daae123c819f968299de46c458da
Itee Mishra <103632132+IteeMishra@users.noreply.github.com>
2024-07-15 00:08:34 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        
       vector<vector<int>>ans;
       for(int i=0;i<nums.size();i++){
        
